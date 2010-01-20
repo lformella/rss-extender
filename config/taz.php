0c67d7404b4914bcc47e0a5a6b639b24ea7ded1b
Rafael Bugajewski <rbugajewski@mac.com>
2010-01-20 13:47:00 +0000

    Adapted taz feed to new site structure. (thanks Bernd)

--- config/taz.php
+++ config/taz.php
@@ -1,11 +1,9 @@
 <?php
-
-$config['author']		= "Rafael Bugajewski";
-$config['author_url']	= "http://www.juicycocktail.com/";
-$config['url']		= "http://www.taz.de/1/rss.xml";
-$config['base_url']	= "http://www.taz.de/";
-$config['content']	= array("#<h1>(.*)<div class=\"plugin_links\">#Uis", 1);
-$config['search']       = array("#<!-- start  smarty\/plugin_morearticles.tmpl -->.*<!--  end   smarty\/plugin_morearticles.tmpl -->#Uis");
-$config['replace']      = array("<div>");
-
+$config['author']     = "Rafael Bugajewski";
+$config['author_url'] = "http://www.juicycocktail.com/";
+$config['url']        = "http://www.taz.de/1/rss.xml";
+$config['base_url']   = "http://www.taz.de/";
+$config['content']    = array("#</h1>(.*)<\/div><div class=\"sectfoot\">#Uis", 1);
+$config['search']     = array("#<!-- start  smarty\/plugin_morearticles.tmpl -->.*<!--  end   smarty\/plugin_morearticles.tmpl -->#Uis");
+$config['replace']    = array("<div>");
 ?>
