e28ed9730d6d91919cccd60739f066cad213ba8a
Lars Formella <ich@larsformella.de>
2010-07-20 10:46:50 +0000

    gulli config was broken due to website changes

--- config/gulli.php
+++ config/gulli.php
@@ -4,21 +4,25 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://ticker.gulli.com/rss/";
 $config['base_url']		= "http://www.gulli.com";
-$config['content']		= array("#<!-- start ContentLeft -->(.*)<!-- end ContentLeft -->#Uis", 1);
+$config['content']		= array("#</h2><p>(.*)</p><p class=\"_redakteur Right P0\">#Uis", 1);
 $config['search']		= array("#<script type='text\/javascript'>.*<\/noscript>#Uis",
 								"#<h4>.*</h4>#Uis",
 								"#<div class=\"BreadCrumb\">.*<div class=\"inside\">#Uis",
 								"#<div class=\"NewsHeading\">.*<\/div>#Uis",
 								"#<!-- AddThis Button BEGIN -->.*<!-- AddThis Button END -->#Uis",
 								"#<div class=\"BreadCrumb\">.*#is",
-								"#<script type=\"text/javascript\">.*</script>#Uis");
+								"#<script type=\"text/javascript\">.*</script>#Uis",
+								"#<a http://exit.gulli.com/url/target=#Uis",												// stupid gulli?!
+								"#<a target=\"_blank\" http://exit.gulli.com/url/http://exit.gulli.com/url/target=#Uis");	// stupid gulli?!
 $config['replace']		= array("",
 								"",
 								"<div>",
 								"",
 								"",
 								"",
-								"");
+								"",
+								"<a target=",
+								"<a target=");
 $config['use_utf8']		= true;
 
 ?>
