4ece80a97f4e188bcdb858a4226ddcd457d3065d
Sven Fischer <strubbl@linux4tw.de>
2010-12-05 00:24:37 +0000

    fix for heise security feed

--- config/heise-sec.php
+++ config/heise-sec.php
@@ -4,7 +4,7 @@ $config['author']		= "Sven Fischer";
 $config['author_url']		= "http://blog.strubbl.de/";
 $config['url']			= "http://www.heise.de/security/news/news-atom.xml";
 $config['base_url']		= "http://www.heise.de";
-$config['content']		= array("#<!--googleon: index-->(.*)<!--googleoff: index-->#Uis", 1);
+$config['content']		= array("#<!--googleon: all-->(.*)<!--googleoff: all-->#Uis", 1);
 $config['search']		= array("#<span class=\"bildunterschrift\">.*<\/span>#Uis",
 								"#<span class=\"source\">.*<\/span>#Uis",
 								"#<h1>.*<\/h1>#Uis",
