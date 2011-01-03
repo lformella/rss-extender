fee2bdca70f46ae391ff0b546639b2f32971c57c
Sven Fischer <strubbl@linux4tw.de>
2011-01-03 12:38:53 +0000

    german-bash feed fixed. rus-cert feed added.

--- config/german-bash.php
+++ config/german-bash.php
@@ -4,7 +4,7 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://feeds2.feedburner.com/gbo-zitate";
 $config['base_url']		= "http://german-bash.org";
-$config['content']		= array("#<div class=\"zitat\">(.*)</div>#Uis", 1);
+$config['content']		= array("#<div class=\"zitat\">(.*)</div>.*<div class=\"pre_next\">#Uis", 1);
 $config['search']		= array("#</span>#Uis");
 $config['replace']		= array("</span><br>");
 $config['cdata_tags']	= array("author");
