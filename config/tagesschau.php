c431e93841f43e2618902360f65ea756e9c543ad
Lars Formella <ich@larsformella.de>
2011-01-27 15:48:39 +0000

    fixed utf8 encoding

--- config/tagesschau.php
+++ config/tagesschau.php
@@ -5,6 +5,5 @@ $config['author_url']	= "http://blog.bernd-distler.net/";
 $config['url']			= "http://www.tagesschau.de/xml/rss2";
 $config['base_url'] 	= "http://www.tagesschau.de";
 $config['content']  	= array("#<span class=\"topline\">(.*)<div class=\"standDatum\">#Uis", 1);
-$config['use_utf8'] 	= true;
 
 ?>
