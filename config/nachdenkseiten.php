015c5244a4b68c56c4450edff80d49a2542c8be6
Sven Fischer <strubbl@linux4tw.de>
2011-02-02 19:17:24 +0000

    nachdenkseiten fix

--- config/nachdenkseiten.php
+++ config/nachdenkseiten.php
@@ -4,8 +4,8 @@ $config['author']		= "Sven Fischer";
 $config['author_url']		= "http://blog.strubbl.de/";
 $config['url']			= "http://www.nachdenkseiten.de/?feed=atom";
 $config['base_url']		= "http://www.nachdenkseiten.de";
-$config['content']		= array("#<div class=\"entry\">(.*)<div class=\"hr_wrap\"><hr /></div>#Uis", 1);
-$config['use_utf8']		= true;
+$config['content']		= array("#<div class=\"entry\">(.*)<p><strong>Vorheriger Beitrag:</strong>#Uis", 1);
+#$config['use_utf8']		= true;
 $config['cdata_tags']		= array("author_name", "link_replies");
 
 ?>
