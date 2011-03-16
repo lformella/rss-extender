0e9e143d889f329b3f270e7ce2173e2eca3fc3f2
Lars Formella <ich@larsformella.de>
2011-03-16 13:46:58 +0000

    fixed configs: spiegel + ubuntugeek

--- config/spiegel_panorama.php
+++ config/spiegel_panorama.php
@@ -5,8 +5,14 @@ $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software
 $config['url']			= "http://www.spiegel.de/panorama/index.rss";
 $config['base_url']		= "http://www.spiegel.de/";
 $config['content']		= array("#<div id=\"spArticleColumn\">(.*)<div id=\"spArticleFunctionsBottom\"#Uis", 1);
-$config['search']		= array("#<div id=\"spArticleFunctions\">.*<\/div>#Uis");
-$config['replace']		= array("");
+$config['search']		= array(	"#<div id=\"spArticleFunctions\">.*<\/div>#Uis",
+									"#<iframe.*</iframe>#Uis",
+									"#<div id=\"spFbTwitterBarTop\">.*<\/div>#Uis",
+									"#<div id=\"spFbTwitterBarInfoTextTop\".*<\/div>#Uis");
+$config['replace']		= array(	"",
+									"",
+									"",
+									"");
 $config['use_utf8']		= true;
 
 ?>
