1dc32a042c75cbfb6753dafe6f0f9e09c203bc60
Sven Fischer <strubbl@linux4tw.de>
2011-05-11 08:16:35 +0000

    pro-linux: stripped down articles to content only (removed social stuff at the end and closing div's at the beginning for a web-compatible view)

--- config/pro-linux.php
+++ config/pro-linux.php
@@ -4,7 +4,7 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.pro-linux.de/NB3/rss/index1.html";
 $config['base_url']		= "http://www.pro-linux.de";
-$config['content']		= array("#<div>Von(.*)(<div id=\"item_nav\">|<div class=\"item_linkbox\">|<!-- Comment main start -->)#Uis", 1);
+$config['content']		= array("#<div>Von\n<a href=\"http://www.pro-linux.de/user/2/(.*)</a>\n</div>\n\n(.*)</div>\n<div class=\"endline\"></div>\n<div id=\"item_npinfo\">#Uis", 2);
 $config['split']		= array("#<a href=\"http://www.pro-linux.de([^\"]*)\" class=\"nav_title\" title=\"nächste\">Nächste</a>#Uis", 1);
 
 ?>
