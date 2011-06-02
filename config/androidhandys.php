801ddc158fec24000698671c8796b403371b2c68
Lars Formella <ich@larsformella.de>
2011-06-02 12:02:48 +0000

    added androidhandys.com feed

---
+++ config/androidhandys.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['author']		= "Lars Formella";
+$config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
+$config['url']			= "http://www.androidhandys.com/feed/";
+$config['base_url']		= "http://www.androidhandys.com";
+$config['content']		= array("#<div class=\"p-con\">(.*)</div>#Uis", 1);
+
+?>
