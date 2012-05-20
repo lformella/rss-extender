186c915ee1d24795302d91c17c8b8957d92337f5
Lars Formella <ich@larsformella.de>
2012-05-20 20:53:41 +0200

    spon fix

--- config/spiegel_panorama.php
+++ config/spiegel_panorama.php
@@ -4,7 +4,7 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.spiegel.de/panorama/index.rss";
 $config['base_url']		= "http://www.spiegel.de/";
-$config['content']		= array("#<div id=\"spArticleColumn\">(.*)<div id=\"spArticleFunctionsBottom\"#Uis", 1);
+$config['content']		= array("#<div id=\"spArticleColumn\"[^>]*>(.*)<div id=\"spArticleFunctionsBottom\"#Uis", 1);
 $config['search']		= array(	"#<div id=\"spArticleFunctions\">.*<\/div>#Uis",
 									"#<iframe.*</iframe>#Uis",
 									"#<div id=\"spFbTwitterBarTop\">.*<\/div>#Uis",
