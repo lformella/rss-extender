42675f1b9cfc274b75c632b64bde240de5fd072d
lightonflux <lightonflux@znn.info>
2012-11-06 16:13:37 +0100

    m

--- config/zeit-digital.php
+++ config/zeit-digital.php
@@ -1,10 +1,10 @@
 <?php
 
 $config['author']		= "lightonflux";
-$config['author_url']	= "http://znn.info";
+$config['author_url']   	= "http://znn.info";
 $config['url']			= "http://newsfeed.zeit.de/digital/index"; // just add the url to rss feed
-$config['base_url']		= "http://http://www.zeit.de/digital/index"; // add the normal url of the page
-$config['content']		= array("#<!--AB HIER IHR CONTENT-->(.*)<div class="articlefooter af">#Uis", 1);
+$config['base_url']		= "http://www.zeit.de/"; // add the normal url of the page
+$config['content']		= array("#<!--AB HIER IHR CONTENT-->(.*)<div class="show_smk" id="smk-plug"> </div>#Uis", 1);
 $config['use_utf8']		= true;
 
 ?>
