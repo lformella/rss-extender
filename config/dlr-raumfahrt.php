e0dac95de06f360ef84b3864253fefdcce977e18
lightonflux <lightonflux@znn.info>
2012-11-06 16:11:49 +0100

    m

--- config/dlr-raumfahrt.php
+++ config/dlr-raumfahrt.php
@@ -3,8 +3,8 @@
 $config['author']		= "lightonflux";
 $config['author_url']	= "lightonflux@znn.info";
 $config['url']			= "http://feeds.feedburner.com/DLR_raumfahrt?format=xml"; // just add the url to rss feed
-$config['base_url']		= "http://http://www.dlr.de"; // add the normal url of the page
-$config['content']		= array("#<div class="article-preview mt20">(.*) </div>#Uis", 1);
+$config['base_url']		= "http://www.dlr.de"; // add the normal url of the page
+$config['content']		= array("#<div class="article-preview mt20">(.*)<div class="article-contact">#Uis", 1);
 $config['use_utf8']		= true;
 
 ?>
