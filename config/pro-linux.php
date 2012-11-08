193ad372153c20d12adcdb496d5fdf8a6c73f361
Sven Fischer <git-dev@linux4tw.de>
2012-11-08 23:08:15 +0100

    this fixes issue #5 in upstream repository

--- config/pro-linux.php
+++ config/pro-linux.php
@@ -2,9 +2,11 @@
 
 $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
-$config['url']			= "http://www.pro-linux.de/NB3/rss/index1.html";
+$config['url']			= "http://www.pro-linux.de/rss/1/3/rss20_alles.xml";
 $config['base_url']		= "http://www.pro-linux.de";
-$config['content']		= array("#<div>Von\n<a href=\"http://www.pro-linux.de/user/2/(.*)</a>\n</div>\n\n(.*)</div>\n<div class=\"endline\"></div>\n<div id=\"item_npinfo\">#Uis", 2);
+$config['content']		= array("#<div class=\"introtext\">(.*)<div class=\"endline\"></div>#Uis", 1);
 $config['split']		= array("#<a href=\"http://www.pro-linux.de([^\"]*)\" class=\"nav_title\" title=\"nächste\">Nächste</a>#Uis", 1);
+$config['search'] 		= array("#<div class=\"item_linkbox\">#Uis");
+$config['replace'] 		= array("");
 
 ?>
