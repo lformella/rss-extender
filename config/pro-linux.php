4b58270ec3e33752c8e3ca2c3276986811b01fcf
Sven Fischer <git-dev@linux4tw.de>
2012-11-15 00:22:22 +0100

    fixes issue #5. pro-linux feed now for news/articles working again

--- config/pro-linux.php
+++ config/pro-linux.php
@@ -2,11 +2,22 @@
 
 $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
-$config['url']			= "http://www.pro-linux.de/rss/1/3/rss20_alles.xml";
+$config['url']			= "http://www.pro-linux.de/NB3/rss/2/3/rss20_aktuell.xml";
 $config['base_url']		= "http://www.pro-linux.de";
 $config['content']		= array("#<div class=\"introtext\">(.*)<div class=\"endline\"></div>#Uis", 1);
 $config['split']		= array("#<a href=\"http://www.pro-linux.de([^\"]*)\" class=\"nav_title\" title=\"nächste\">Nächste</a>#Uis", 1);
-$config['search'] 		= array("#<div class=\"item_linkbox\">#Uis");
-$config['replace'] 		= array("");
+$config['search'] 		= array("#<div class=\"item_linkbox\">#Uis",
+	"#<a href=\"javascript:(.*)\"><img(.*)></a>#Uis",
+	"#<div class=\"picto\">(.*)</div>#Uis",
+	"#<div class=\"imgartbox\" style=\"width:(.*)px;float:right;\">(.*)</div>#Uis",
+	"#<div(.*)>#Uis",
+	"#</div>#Uis");
+$config['replace'] 		= array("",
+	"<img$2>",
+	"",
+	"$2",
+	"",
+	""
+	);
 
 ?>
