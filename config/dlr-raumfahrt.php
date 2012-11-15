e94f725ccd3e2908afe712df8ef309685ba113c8
lightonflux <lightonflux@znn.info>
2012-11-16 00:11:47 +0100

    zeichen richtig escaped

--- config/dlr-raumfahrt.php
+++ config/dlr-raumfahrt.php
@@ -4,7 +4,7 @@ $config['author']		= "lightonflux";
 $config['author_url']	= "http://znn.info";
 $config['url']			= "http://feeds.feedburner.com/DLR_raumfahrt?format=xml"; // just add the url to rss feed
 $config['base_url']		= "http://www.dlr.de"; // add the normal url of the page
-$config['content']		= array("#<div class="article-preview mt20">(.*)<div class="article-contact">#Uis", 1);
+$config['content']		= array("#<div class=\"article-preview mt20\">(.*)<div class=\"article-contact\">#Uis", 1);
 $config['use_utf8']		= true;
 
 ?>
