bd4c9bbb6d5ec040a521e2d77b0a3c0d92fc233a
markus <markus@virgo.(none)>
2012-11-21 15:55:50 +0100

    Remove taz author information as well as additional links section in articles

--- config/taz.php
+++ config/taz.php
@@ -4,6 +4,15 @@ $config['author_url'] = "http://www.juicycocktail.com/";
 $config['url']        = "http://www.taz.de/1/rss.xml";
 $config['base_url']   = "http://www.taz.de/";
 $config['content']    = array("#</h1>(.*)<\/div><div class=\"sectfoot\">#Uis", 1);
-$config['search']     = array("#<!-- start  smarty\/plugin_morearticles.tmpl -->.*<!--  end   smarty\/plugin_morearticles.tmpl -->#Uis");
-$config['replace']    = array("<div>");
+// search[i] match is replaced with corresponding replace[i]
+$config['search']     = array("#<!-- start  smarty\/plugin_morearticles.tmpl -->.*<!--  end   smarty\/plugin_morearticles.tmpl -->#Uis", 
+		"#<div class=\"sect sect_shorty box first_box last_box\" role=\"region\">.*<p class=\"article\">#Uis",
+		"#<div class=\"sect sect_author box first_box last_box\" role=\"region\">.*<p class=\"article\">#Uis",
+		"#<div class=\"sect sect_seealso box first_box last_box\" role=\"region\">.*<p class=\"article\">#Uis",
+		"#<div class=\"rack\">#Uis");
+$config['replace']    = array("<div>", 
+		"<p class=\"article\">",
+		"<p class=\"article\">",
+		"<p class=\"article\">",
+		"");
 ?>
