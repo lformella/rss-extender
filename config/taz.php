f23a9de88b27d80713d76b0616e6e033e20ac580
Markus Alexander Kuppe <github.com@lemmster.de>
2012-12-06 11:13:30 +0100

    Filter taz paywall

--- config/taz.php
+++ config/taz.php
@@ -9,10 +9,14 @@ $config['search']     = array("#<!-- start  smarty\/plugin_morearticles.tmpl -->
 		"#<div class=\"sect sect_shorty box first_box last_box\" role=\"region\">.*<p class=\"article\">#Uis",
 		"#<div class=\"sect sect_author box first_box last_box\" role=\"region\">.*<p class=\"article\">#Uis",
 		"#<div class=\"sect sect_seealso box first_box last_box\" role=\"region\">.*<p class=\"article\">#Uis",
-		"#<div class=\"rack\">#Uis");
+		"#<div class=\"rack\">#Uis",
+		"#<div class=\"anchor anchor_tzi_paywall\" id=\"tzi_paywall\">.*</p></div></div></div></div></div>#Uis",
+		"#<script.*/script>#Uis");
 $config['replace']    = array("<div>", 
 		"<p class=\"article\">",
 		"<p class=\"article\">",
 		"<p class=\"article\">",
+		"",
+		"",
 		"");
 ?>
