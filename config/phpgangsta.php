25e51c0d4b9001d6e9801a600e294837f76d7499
Lars Formella <ich@larsformella.de>
2012-12-09 00:03:16 +0100

    add phpgangsta

---
+++ config/phpgangsta.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['author']		= "Lars Formella";
+$config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
+$config['url']			= "http://www.phpgangsta.de/feed";
+$config['base_url']		= "http://www.phpgangsta.de";
+$config['content']		= array("#<div class=\"main\">(.*)<div#Uis", 1);
+
+?>
