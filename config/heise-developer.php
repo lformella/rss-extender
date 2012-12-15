c3ad88619dfec59a5cfbda4e929275f0837f472b
Sven Fischer <git-dev@linux4tw.de>
2012-12-15 19:14:42 +0100

    added heise developer feed

---
+++ config/heise-developer.php
@@ -0,0 +1,9 @@
+<?php
+
+include(__DIR__ . "/heise.php");
+
+$config['author']		= "Sven Fischer";
+$config['author_url']	= "http://blog.strubbl.de/";
+$config['url']			= "http://www.heise.de/developer/rss/news-atom.xml";
+
+?>
