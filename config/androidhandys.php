315ebcc094caa155e237d8af9acc37fbc61e8a89
Lars Formella <ich@larsformella.de>
2013-05-01 15:41:21 +0200

    fix some feeds

--- config/androidhandys.php
+++ config/androidhandys.php
@@ -4,6 +4,6 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.androidhandys.com/feed/";
 $config['base_url']		= "http://www.androidhandys.com";
-$config['content']		= array("#<div class=\"p-con\">(.*)</div>#Uis", 1);
+$config['content']		= array("#<div class=\"entry-content\">(.*)</div>#Uis", 1);
 
 ?>
--- config/ubuntugeek.php
+++ config/ubuntugeek.php
@@ -4,10 +4,8 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://feeds.feedburner.com/UbuntuGeek";
 $config['base_url']		= "http://www.ubuntugeek.com";
-$config['content']		= array("#<div class=\"text\">(.*)<div class=\"addtoany_share_save_container\">#Uis", 1);
-$config['search']		= array("#<script.*>([^<]*)<\/script>#Uis",
-								"#<a name=\"fb_share\".*</a></div>#Uis");
-$config['replace']		= array("",
-								"");
+$config['content']		= array("#</center>(.*)<center>#Uis", 1);
+$config['search']		= array("#<script.*><\/script>#Uis");
+$config['replace']		= array("");
 
 ?>
