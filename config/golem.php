9bbebb747af2da120e8da72ab1c64a00edbcc319
Lars Formella <github@larsf.de>
2013-07-03 13:37:04 +0200

    Update golem.php

--- config/golem.php
+++ config/golem.php
@@ -5,20 +5,15 @@ $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software
 $config['url']			= "http://rss.golem.de/rss.php?feed=RSS2.0";
 $config['base_url']		= "http://www.golem.de";
 $config['content']		= array("#<!--content-->(.*)<!--/content-->#Uis", 1);
-$config['search']		= array("#<script type=\"text\/javascript\">.*<!--.* = new.*SWFObject\(\"(.*)\",\".*\",\"(.*)\",\"(.*)\",\".*\",\".*\"\);.*.addVariable\(\"id\", \"(.*)\"\);.*<\/script>#Uis",
+$config['search']		= array("#<script type=\"text\/javascript\">.*= new.*SWFObject\(\"(.*)\",\".*\",\"(.*)\",\"(.*)\",\".*\",\".*\"\);.*.addVariable\(\"id\", \"(.*)\"\);.*<\/script>#Uis",
 								"#<script( language=\"javascript\"|) type=\"text\/javascript\".*>.*<\/script>#Uis",
 								"#<li class=\".*\">(.*)<\/li>#Uis",
 								"#<ul class=\".*\">(.*)<\/ul>#Uis",
-								"#<div class=\"gg_embeddedIndex gg_solid\">.*<\/div>#Uis",
-								"#<div class=\"gg_toOldGallery\">.*<\/div>#Uis",
-								"#<div class=\"gg_embeddedSub.*\">.*<\/div>#Uis"
-);
+								"#<li>\s*<img src=\"\"[^>]*>\s*</li>#Uis");
 $config['replace']		= array("<embed type=\"application/x-shockwave-flash\" src=\"$1\" bgcolor=\"#000000\" quality=\"high\" allowfullscreen=\"true\" scale=\"noScale\" allowscriptaccess=\"always\" flashvars=\"id=$4&amp;qual=medium&amp;autoPl=false&amp;buffer=6\" width=\"$2\" height=\"$3\">",
 								"",
 								"$1",
 								"$1",
-								"",
-								"",
 								"");
 $config['split']		= array("#<link rel=\"next\" href=\"([^\"]*)\">#Uis", 1);
 
