73947c3c7a396bf8067a70a37e9d763d2ad48b47
Lars Formella <ich@larsformella.de>
2013-10-11 19:28:52 +0200

    closes #22 (add heise mac+i)

---
+++ config/heise-mac.php
@@ -0,0 +1,9 @@
+<?php
+
+include(__DIR__ . "/heise.php");
+
+$config['author']		= "Lars Formella";
+$config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
+$config['url']			= "http://www.heise.de/mac-and-i/news-atom.xml";
+
+?>
