0f74ab8f3245e3a07aafab58231c0dacda03500f
Lars Formella <ich@larsformella.de>
2014-09-21 15:52:48 +0200

    fix heise images

--- config/heise.php
+++ config/heise.php
@@ -4,20 +4,22 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.heise.de/newsticker/heise-atom.xml";
 $config['base_url']		= "http://www.heise.de";
-$config['content']		= array("#<div class=\"(meldung_wrapper|output_text)\">(.*)</div>(\s+</article>)?(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->#Uis", 2);
+$config['content']		= array("#<div class=\"(meldung_wrapper|output_text)\">(.*)</div>(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->#Uis", 2);
 $config['search']		= array("#<span class=\"bildunterschrift\">.*<\/span>#Uis",
 								"#<span class=\"source\">.*<\/span>#Uis",
 								"#<h1>.*<\/h1>#Uis",
 								"#<!-- [a-z_-]+ -->#Uis",
 								"#<script.*>.*<\/script>#Uis",
 								"#<noscript>.*<\/noscript>#Uis",
-								"#<div class=\"video_titel\">[^<]*<\/div>#Uis");
+								"#<div class=\"video_titel\">[^<]*<\/div>#Uis",
+								"#http://www.heise.de//#Uis");
 $config['replace']		= array("",
 								"",
 								"",
 								"",
 								"",
 								"",
-								"");
+								"",
+								"//");
 
 ?>
