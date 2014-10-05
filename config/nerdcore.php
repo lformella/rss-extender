0e7a61093b18709b1701a54a215c67b7dc6c2e08
Sven Fischer <git-dev@linux4tw.de>
2014-10-05 13:57:54 +0200

    reworks nerdcore feed to be faster with less crap
    more precise regex for content to be faster when searching for matches
    removes social button crap
    removes disqus crap

--- config/nerdcore.php
+++ config/nerdcore.php
@@ -4,6 +4,11 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://feeds2.feedburner.com/NerdcoreRSS2";
 $config['base_url']		= "http://www.nerdcore.de";
-$config['content']		= array("#<div class=\"entry-content\">(.*)</div>#Uis", 1);
+$config['content']		= array("#<div class=\"entry-content\">(.*)</div> <!-- CLOSING ENTRY CONTENT -->#Uis", 1);
+
+$config['search'] =  array("#<div id=\"socialbuttons\">.*</div> <!-- CLOSING SOCIALBUTTONS -->#Uis",
+                           "#&middot; <span class=\"commentscommentscomments\"><a href=.*Comments loading…</a></span>#Uis");
+$config['replace'] = array("",
+                           "");
 
 ?>
