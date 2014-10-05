0c541ca49f8db83464453ef00d2dfebc76a08129
Sven Fischer <git-dev@linux4tw.de>
2014-10-05 14:28:42 +0200

    repairs nachdenkseiten feed

--- config/nachdenkseiten.php
+++ config/nachdenkseiten.php
@@ -1,9 +1,12 @@
 <?php
 
-$config['author']		= "Sven Fischer";
-$config['author_url']	= "http://blog.strubbl.de/";
-$config['url']			= "http://www.nachdenkseiten.de/?feed=atom";
-$config['base_url']		= "http://www.nachdenkseiten.de";
-$config['content']		= array("#<div class=\"entry\">(.*)<p><strong>Vorheriger Beitrag:</strong>#Uis", 1);
+$config['author']     = "Sven Fischer";
+$config['author_url'] = "http://blog.strubbl.de/";
+$config['url']        = "http://www.nachdenkseiten.de/?feed=rss2";
+$config['base_url']   = "http://www.nachdenkseiten.de";
+$config['content']    = array("#<div class=\"entry single\">(.*)</div>\s*<!-- spendenfoerdern - BEGIN -->#Uis", 1);
+
+$config['search']     = array("#<small>\s*<span class=\"upTop\">.*</small>#Uis");
+$config['replace']    = array("");
 
 ?>
