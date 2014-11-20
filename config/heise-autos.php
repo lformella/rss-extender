e342c8e16fa2b4b110f7dc764176354f831694ae
Sven Fischer <git-dev@linux4tw.de>
2014-11-20 18:28:13 +0100

    overwrites content regex in heise-autos config

--- config/heise-autos.php
+++ config/heise-autos.php
@@ -5,5 +5,6 @@ include(__DIR__ . "/heise.php");
 $config['author']		= "Sven Fischer";
 $config['author_url']	= "http://blog.strubbl.de/";
 $config['url']			= "http://www.heise.de/autos/rss/news-atom.xml";
+$config['content'] = array("#<section id=\"artikel_text\">\s*<!--googleon: index-->(.*)<!--googleoff: index-->#Uis", 1);
 
 ?>
