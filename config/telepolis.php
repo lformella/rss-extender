245df722590c779e7a0e2136c60050a1282735ce
Sven Fischer <git-dev@linux4tw.de>
2015-05-07 21:17:14 +0200

    fix telepolis

--- config/telepolis.php
+++ config/telepolis.php
@@ -2,7 +2,7 @@
 include(__DIR__ . "/heise.php");
 
 $config['url']			= "http://www.heise.de/tp/news.rdf";
-$config['content']  = array("#<div class=\"mar0\">(.*)<div id=\"breadcrumb\">#Uis", 1);
+$config['content']  = array("#<div class=\"mar0\">(.*)<!--START-bottom-leiste-->#Uis", 1);
 #$config['split']    = array("#<a href=\"([^\"]*)\" class=\"page next\" rel=\"next\">Nächste Seite</a>#Uis", 1); #split does not work here due to crippled website. we would need guid and 1st match here to create link to second page
 array_push($config['search'],  "#<span class=\"vcard\" id=.*</span>\s*</div>#Uis");
 array_push($config['replace'], "");
