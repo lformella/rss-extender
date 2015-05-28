4a46c23f0431005f039209b5b5f13563180a5f05
Sven Fischer <git-dev@linux4tw.de>
2015-05-28 17:40:28 +0200

    add extender for dilbert.com

---
+++ config/dilbert.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['author']		= "Sven Fischer";
+$config['author_url']	= "http://blog.strubbl.de/";
+$config['url']			= "http://dilbert.com/feed";
+$config['base_url']		= "http://dilbert.com";
+$config['content']		= array("#<div class=\"img-comic-container\">(.*)<meta itemprop=\"isFamilyFriendly\"#Uis", 1);
+
+?>
