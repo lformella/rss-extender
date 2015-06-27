6451205feb7a8488604b72a0426c7d209b01a550
Sven Fischer <git-dev@linux4tw.de>
2015-06-28 00:38:18 +0200

    fix search regex arrays in der-postillion.php and rus-cert.php for fixing #25

--- config/der-postillion.php
+++ config/der-postillion.php
@@ -5,7 +5,7 @@ $config['author_url']	= "http://znn.info";
 $config['url']			= "http://feeds.feedburner.com/blogspot/rkEL?format=xml"; // just add the url to rss feed
 $config['base_url']             = "http://www.der-postillon.com"; // add the normal url of the page
 $config['content']              = array("#<h3 class=\'post-title entry-title\' itemprop=\'name\'>(.*)<div class=\'post-footer\'>#Uis", 1);
-$config['search	']		= array("#<ads>.*<\/ads>#Uis",);
+$config['search	']		= array("#<ads>.*<\/ads>#Uis");
 $config['replace']		= array("");
 
 ?>
--- config/rus-cert.php
+++ config/rus-cert.php
@@ -5,10 +5,5 @@ $config['author_url']		= "http://blog.strubbl.de/";
 $config['url']			= "http://cert.uni-stuttgart.de/ticker/rus-cert.xml";
 $config['base_url']		= "http://cert.uni-stuttgart.de";
 $config['content']		= array("#<h1>RUS-CERT-Meldung</h1>(.*)<P><B>Hinweis</B><BR>#Uis", 1);
-$config['search']		= array("");
-$config['replace']		= array("",
-								"",
-								"",
-								"");
 
 ?>
