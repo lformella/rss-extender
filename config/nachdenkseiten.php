b90b6fb1b60b0b535b3b7369fd22f2a47f634c10
Sven Fischer <git-dev@linux4tw.de>
2015-07-06 19:24:53 +0200

    fix nachdenkseiten entry regex

--- config/nachdenkseiten.php
+++ config/nachdenkseiten.php
@@ -4,7 +4,7 @@ $config['author']     = "Sven Fischer";
 $config['author_url'] = "http://blog.strubbl.de/";
 $config['url']        = "http://www.nachdenkseiten.de/?feed=rss2";
 $config['base_url']   = "http://www.nachdenkseiten.de";
-$config['content']    = array("#<div class=\"entry single\">(.*)</div>\s*<!-- spendenfoerdern - BEGIN -->#Uis", 1);
+$config['content']    = array("#<div class=\"entry single\">(.*)</div>\s*<div class=\"spendenfoerdern\">#Uis", 1);
 
 $config['search']     = array("#<small>\s*<span class=\"upTop\">.*</small>#Uis");
 $config['replace']    = array("");
