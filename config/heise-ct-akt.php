9aeb4e583995c25ac87931aa319c0002a1e92ef6
Sven Fischer <git-dev@linux4tw.de>
2015-07-21 20:25:16 +0200

    update content regex for heise-ct
    extend heise content regex with special regex for heise-ct because of
    the mixture of different article types

--- config/heise-ct-akt.php
+++ config/heise-ct-akt.php
@@ -5,5 +5,6 @@ include(__DIR__ . "/heise.php");
 $config['author']		= "Sven Fischer";
 $config['author_url']	= "http://blog.strubbl.de/";
 $config['url']			= "http://www.heise.de/ct/rss/artikel-atom.xml";
+$config['content'] = array("#(<div class=\"(meldung_wrapper|output_text)\">|<div id=\"artikel\".*\">)(.*)(</article>|<div class=\"clear\"></div>\s</div>|<\/div>(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->)#Us", 3);
 
 ?>
