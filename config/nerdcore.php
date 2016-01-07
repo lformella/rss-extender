efb81e9a6caabeee5e9b532ae982510f958950ab
daniel <daniel.niedermeyer@stud.hs-regensburg.de>
2016-01-07 22:45:55 +0100

    Fixed nerdcore.php - again

--- config/nerdcore.php
+++ config/nerdcore.php
@@ -4,15 +4,8 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.nerdcore.de/feed/";
 $config['base_url']		= "http://www.nerdcore.de";
-$config['content']		= array("#<div class=\"entry-content\">(.*)</div>#Uis", 1);
+$config['content']		= array("#<div class=\"entry-content\">(.*)<div class=\"entry-footer\">#Uis", 1);
 
-$config['search'] =  array("#<div id=\"socialbuttons\">.*</div>#Uis",
-                           "#&middot; <span class=\"commentscommentscomments\"><a href=.*Comments loading…</a></span>#Uis",
-                           "#<h1 class=\"entry-title\">.*</h1>#Uis",
-                           "#<p class=\"postmeta\">.*</p>#Uis");
-$config['replace'] = array("",
-                           "",
-                           "",
-                           "");
-
+$config['search'] =  array("#<h1 class=\"entry-title\">.*</h1>#Uis");
+$config['replace'] = array("");
 ?>
