93d5d001099652503e341a86d84fbe0e6929491e
daniel <daniel.niedermeyer@stud.hs-regensburg.de>
2016-01-21 09:37:45 +0100

    nerdcore regex fix
    adjusted the regex to the new nerdcore.de layout

--- config/nerdcore.php
+++ config/nerdcore.php
@@ -4,8 +4,5 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.nerdcore.de/feed/";
 $config['base_url']		= "http://www.nerdcore.de";
-$config['content']		= array("#<div class=\"entry-content\">(.*)<div class=\"entry-footer\">#Uis", 1);
-
-$config['search'] =  array("#<h1 class=\"entry-title\">.*</h1>#Uis");
-$config['replace'] = array("");
+$config['content']		= array("#<div class=\"postbody\">(.*)</div>[\n\r\s]*</div>[\n\r\s]*<!-- Load Disqus on Demand -->#Uis", 1);
 ?>
