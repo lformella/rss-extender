541f6ea07570389bf3598e2ba7abc2cb734b4d7c
Sven Fischer <git-dev@linux4tw.de>
2016-01-22 19:55:09 +0100

    nerdcore: remove lazy loading of images for web rss readers

--- config/nerdcore.php
+++ config/nerdcore.php
@@ -5,4 +5,10 @@ $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software
 $config['url']			= "http://www.nerdcore.de/feed/";
 $config['base_url']		= "http://www.nerdcore.de";
 $config['content']		= array("#<div class=\"postbody\">(.*)</div>[\n\r\s]*</div>[\n\r\s]*<!-- Load Disqus on Demand -->#Uis", 1);
+$config['search'] =  array( "#<img.*data-lazy-type=\"image\".*<noscript>#Ui",
+                            "#<\/noscript>#Ui"
+                          );
+$config['replace'] = array( "",
+                            ""
+                          );
 ?>
