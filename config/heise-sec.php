6d6faf4a501cad7813bf3fb92fbeaefffe5476e8
Sven Fischer <git-dev@linux4tw.de>
2016-02-06 00:35:52 +0100

    add test urls to heise security config

--- config/heise-sec.php
+++ config/heise-sec.php
@@ -5,5 +5,6 @@ include(__DIR__ . "/heise.php");
 $config['author']		= "Sven Fischer";
 $config['author_url']	= "http://blog.strubbl.de/";
 $config['url']			= "http://www.heise.de/security/news/news-atom.xml";
+$config['test_urls'] = [ "http://heise.de/-3096123", "http://heise.de/-3095739" ];
 
 ?>
