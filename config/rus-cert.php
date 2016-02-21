416b6fa84e9c29ac3a838e8fdb860cb5db0f7c9d
Sven Fischer <git-dev@linux4tw.de>
2016-02-21 22:34:43 +0100

    rus-cert: fix content regex

--- config/rus-cert.php
+++ config/rus-cert.php
@@ -4,7 +4,7 @@ $config['author']		= "Sven Fischer";
 $config['author_url']		= "http://blog.strubbl.de/";
 $config['url']			= "http://cert.uni-stuttgart.de/ticker/rus-cert.xml";
 $config['base_url']		= "http://cert.uni-stuttgart.de";
-$config['content']		= array("#<h1>RUS-CERT-Meldung</h1>(.*)<P><B>Hinweis</B><BR>#Uis", 1);
+$config['content']		= array("#<div id=\"main\">(.*)<P>Copyright &copy;#Uis", 1);
 $config['test_urls'] = array("https://cert.uni-stuttgart.de/ticker/article.php?mid=1740",
                              "https://cert.uni-stuttgart.de/ticker/article.php?mid=1728"
                             );
