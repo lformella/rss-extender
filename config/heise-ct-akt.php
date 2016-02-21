059f2e8937efbff2b20c7525df2b9163c86aeb87
Sven Fischer <git-dev@linux4tw.de>
2016-02-21 22:20:15 +0100

    heise-ct-akt: fix content regex for several special pages

--- config/heise-ct-akt.php
+++ config/heise-ct-akt.php
@@ -5,6 +5,7 @@ include(__DIR__ . "/heise.php");
 $config['author']		= "Sven Fischer";
 $config['author_url']	= "http://blog.strubbl.de/";
 $config['url']			= "http://www.heise.de/ct/rss/artikel-atom.xml";
-$config['content'] = array("#(<div class=\"(meldung_wrapper|output_text)\">|<div id=\"artikel\".*\">)(.*)(</article>|<div class=\"clear\"></div>\s</div>|<\/div>(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->)#Us", 3);
+$config['content'] = array("#(<div class=\"(article_page_text|meldung_wrapper|output_text)\">|<div id=\"artikel\".*\">|<section>)(.*)(</article>|<div class=\"clear\"></div>\s</div>|<\/div>(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->|</section>)#Uis", 3);
+$config['test_urls'] = array("http://heise.de/-3100718", "http://heise.de/-3090337");
 
 ?>
