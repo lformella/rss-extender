e98682394f7f682bfd8e679385899aa3cc0aef7f
Sven Fischer <git-dev@linux4tw.de>
2016-02-21 22:38:00 +0100

    postillon: fix content regex to include Sonntagsumfrage, too

--- config/der-postillon.com.php
+++ config/der-postillon.com.php
@@ -5,8 +5,7 @@ $config['author_url'] = "https://www.lemmster.de/";
 $config['url']        = "http://feeds.feedburner.com/blogspot/rkEL";
 $config['base_url']   = "http://der-postillon.com";
 
-//$config['content']    = array("#<div class='post-body entry-content' id='.*' itemprop='articleBody'>(.*)<div class=\"separator\" style=\"clear: both; text-align: center;\"></div>#Uis", 1);
-$config['content']    = array("#<div class='post-body entry-content' id='.*' itemprop='articleBody'>(.*)<div style='clear: both;'></div>#Uis", 1);
+$config['content']    = array("#<div class='post-body entry-content.*itemprop='articleBody'>(.*)<div style='clear: both;'></div>#Uis", 1);
 
 /* search[i] match is replaced with corresponding replace[i] */
 //$config['search']     = array("");
