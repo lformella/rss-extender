0b4b68854d6313fe3a26660741dadd43c94678b4
Sven Fischer <git-dev@linux4tw.de>
2016-02-21 21:33:28 +0100

    nerdcore: fix content regex

--- config/nerdcore.php
+++ config/nerdcore.php
@@ -4,7 +4,7 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.nerdcore.de/feed/";
 $config['base_url']		= "http://www.nerdcore.de";
-$config['content']		= array("#<div class=\"postbody\">(.*)</div>[\n\r\s]*</div>[\n\r\s]*<!-- Load Disqus on Demand -->#Uis", 1);
+$config['content']		= array("#<div class=\"postbody\">(.*)</div>\s*</div>\s*<!-- Disqus Desktop -->\s*<div id=\"dsqloader\"></div>#Uis", 1);
 $config['search'] =  array( "#<img.*data-lazy-type=\"image\".*<noscript>#Ui",
                             "#<\/noscript>#Ui"
                           );
