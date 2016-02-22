10559916770472dade0bebc869d08a5cca56372d
Carsten Braune <cb@carsten-braune.de>
2016-02-22 17:21:09 +0100

    added feeds for tagesspiegel.de

---
+++ config/tagesspiegel_wirtschaft.php
@@ -0,0 +1,9 @@
+<?php
+
+include(__DIR__ . "/tagesspiegel.php");
+
+$config['author']		= "Carsten Braune";
+$config['author_url']	= "http://www.carsten-braune.de";
+$config['url']			= "http://www.tagesspiegel.de/contentexport/feed/wirtschaft";
+
+?>
