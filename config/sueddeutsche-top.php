6f6a656d248cd7c9e0746fc85d95c2eadcb89b0b
Sven Fischer <git-dev@linux4tw.de>
2016-08-16 14:45:10 +0200

    fix sueddeutsche-top
    This commit fixes #34. Multi-page articles work, teasers and ads are
    stripped off content.

--- config/sueddeutsche-top.php
+++ config/sueddeutsche-top.php
@@ -4,6 +4,19 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://rss.sueddeutsche.de/rss/Topthemen";
 $config['base_url']		= "http://www.sueddeutsche.de";
-$config['content']		= array("#<div id=\"contentcolumn\" class=\"entry-content\" role=\"main\">(.*)<p class=\"quelle\">#Uis", 1);
+$config['content']		= array("#<section class=\"body\" id=\"article-body\">(.*)<section class=\"article-info\">#Uis", 1);
+
+$config['search'] = array("#<div class=\"ad\".*<!-- END ad tag.* -->#Uis",
+  "#<figure class=\"teaserable.*</figure>#Uis",
+  "#<div id=\"article-sidebar-wrapper\".*</div>\s</div>#Uis",
+  "#<section class=\"authors\">.*</section>#Uis",
+  "#<script.*</script>#Uis");
+$config['replace'] = array("",
+                           "",
+                           "",
+                           "",
+                           "");
+
+$config['split'] = array("#<a href=\"http://www.sueddeutsche.de([^\"]*)\" class=\"article-paging-nav-btn\">#Uis", 1);
 
 ?>
