067e65e5c75b30d1c2e25c11800c88fb2a2bf65a
Sven Fischer <git-dev@linux4tw.de>
2016-11-04 16:10:56 +0100

    fix telepolis

--- config/telepolis.php
+++ config/telepolis.php
@@ -1,13 +1,14 @@
 <?php
 include(__DIR__ . "/heise.php");
 
-$config['url']			= "http://www.heise.de/tp/news.rdf";
-$config['content']  = array("#<div class=\"mar0\">(.*)<!--START-bottom-leiste-->#Uis", 1);
+$config['url']			= "http://www.heise.de/tp/news-atom.xml";
+$config['content']  = array("#(<figure class=\"aufmacherbild\">|<p class=\"lead beitraganriss\">)(.*)<footer class=\"beitragsfooter\">#Uis", 2);
 #$config['split']    = array("#<a href=\"([^\"]*)\" class=\"page next\" rel=\"next\">Nächste Seite</a>#Uis", 1); #split does not work here due to crippled website. we would need guid and 1st match here to create link to second page
-array_push($config['search'],  "#<span class=\"vcard\" id=.*</span>\s*</div>#Uis");
-array_push($config['replace'], "");
-$config['test_urls'] = array("http://www.heise.de/tp/artikel/47/47452/1.html",
-                             "http://www.heise.de/tp/news/Erdgas-EU-Kommission-will-Notfallplaene-3112645.html"
-                            );
+$config['search'] = array("#srcset=\".*>#Uis");
+$config['replace'] = array(">");
 
+$config['test_urls'] = array("http://www.heise.de/tp/artikel/47/47452/1.html",
+  "http://www.heise.de/tp/news/Erdgas-EU-Kommission-will-Notfallplaene-3112645.html",
+  "http://heise.de/-3455305"
+                            );
 ?>
