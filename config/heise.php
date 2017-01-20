e2303b272f3398a3502a0d845194e3116b3542dd
Sven Fischer <git-dev@linux4tw.de>
2017-01-20 16:40:38 +0100

    fix heise-hardware/make

--- config/heise.php
+++ config/heise.php
@@ -4,7 +4,7 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.heise.de/newsticker/heise-atom.xml";
 $config['base_url']		= "http://www.heise.de";
-$config['content']		= array("#<div class=\"(meldung_wrapper|output_text|kicker_toc)\">(.*)</div>(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->#Uis", 2);
+$config['content']		= array("#<div class=\"(meldung_wrapper|output_text|kicker_toc|article-content)\">(.*)</div>(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->#Uis", 2);
 $config['search']		= array("#<span class=\"bildunterschrift\">.*<\/span>#Uis",
 								"#<span class=\"source\">.*<\/span>#Uis",
 								"#<h1>.*<\/h1>#Uis",
