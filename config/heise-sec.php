3e081d13ee309083b24f68959eaa47688d5ee5da
Sven Fischer <git-dev@linux4tw.de>
2018-01-31 18:46:50 +0100

    fix heise security rss url

--- config/heise-sec.php
+++ config/heise-sec.php
@@ -4,7 +4,7 @@ include(__DIR__ . "/heise.php");
 
 $config['author']		= "Sven Fischer";
 $config['author_url']	= "http://blog.strubbl.de/";
-$config['url']			= "http://www.heise.de/security/news/news-atom.xml";
+$config['url']			= "https://www.heise.de/security/rss/news-atom.xml";
 $config['test_urls'] = [ "http://heise.de/-3096123", "http://heise.de/-3095739" ];
 
 ?>
