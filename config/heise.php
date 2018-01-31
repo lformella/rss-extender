a321e52703b5b283c6fef1819431e120866294b4
Sven Fischer <git-dev@linux4tw.de>
2018-01-31 18:46:50 +0100

    fix heise content fetching

--- config/heise.php
+++ config/heise.php
@@ -4,7 +4,7 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.heise.de/newsticker/heise-atom.xml";
 $config['base_url']		= "http://www.heise.de";
-$config['content']		= array("#<div class=\"(meldung_wrapper|output_text|kicker_toc|article-content)\">(.*)</div>(\s+<!-- RSPEAK_STOP -->)?\s+<!--googleoff: all-->#Uis", 2);
+$config['content']		= array("#<(div|p) class=\"(meldung_wrapper|output_text|kicker_toc|article-content|article_page_intro)\">(.*)</(div|p)>\s+(<!-- RSPEAK_STOP -->\s+)?(</section>\s+)?<footer class=\"beitragsfooter\">#Uis", 3);
 $config['search']		= array("#<span class=\"bildunterschrift\">.*<\/span>#Uis",
 								"#<span class=\"source\">.*<\/span>#Uis",
 								"#<h1>.*<\/h1>#Uis",
