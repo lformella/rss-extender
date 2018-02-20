362e0416dce352690d7e525004cd0d2233243983
Sven Fischer <git-dev@linux4tw.de>
2018-02-20 15:11:02 +0100

    fix heise ix feed url

--- config/heise-ix.php
+++ config/heise-ix.php
@@ -4,7 +4,7 @@ include(__DIR__ . "/heise.php");
 
 $config['author']		= "Sven Fischer";
 $config['author_url']	= "http://blog.strubbl.de/";
-$config['url']			= "http://www.heise.de/ix/news/news-atom.xml";
+$config['url']			= "http://www.heise.de/ix/rss/news-atom.xml";
 $config['test_urls'] = array("http://heise.de/-3113007", "http://heise.de/-3110745");
 
 ?>
