2284df026862fd25062347b3dcbd1903b0736852
Sven Fischer <git-dev@linux4tw.de>
2018-02-20 15:03:48 +0100

    remove heise ads

--- config/heise.php
+++ config/heise.php
@@ -6,22 +6,24 @@ $config['url']			= "http://www.heise.de/newsticker/heise-atom.xml";
 $config['base_url']		= "http://www.heise.de";
 $config['content']		= array("#<(div|p) class=\"(meldung_wrapper|output_text|kicker_toc|article-content|article_page_intro)[^>]*>(.*)</(div|p)>\s+(<!-- RSPEAK_STOP -->\s+)?(</section>\s+)?<footer#Uis", 3);
 $config['search']		= array("#<span class=\"bildunterschrift\">.*<\/span>#Uis",
-								"#<span class=\"source\">.*<\/span>#Uis",
-								"#<h1>.*<\/h1>#Uis",
-								"#<!-- [a-z_-]+ -->#Uis",
-								"#<script.*>.*<\/script>#Uis",
-								"#<noscript>.*<\/noscript>#Uis",
-								"#<div class=\"video_titel\">[^<]*<\/div>#Uis",
-								"#<section class=\"themenseite_preisvergleich.*<\/section>#Uis",
-								"#http://www.heise.de//#Uis");
+  "#<(div|aside)[ ]*class=\"[^\"]*(akwa-ad-container|bcadv).*<\/(noscript|aside)>#Uis",
+  "#<span class=\"source\">.*<\/span>#Uis",
+  "#<h1>.*<\/h1>#Uis",
+  "#<!-- [a-z_-]+ -->#Uis",
+  "#<script.*>.*<\/script>#Uis",
+  "#<noscript>.*<\/noscript>#Uis",
+  "#<div class=\"video_titel\">[^<]*<\/div>#Uis",
+  "#<section class=\"themenseite_preisvergleich.*<\/section>#Uis",
+  "#http://www.heise.de//#Uis");
 $config['replace']		= array("",
-								"",
-								"",
-								"",
-								"",
-								"",
-								"",
-								"",
-								"//");
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "//");
 
 ?>
