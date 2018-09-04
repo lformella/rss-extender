6b08a9772d92f81d523dc1fa701e88cf37d54fef
Steffen Schröder <steffen.schroeder@aklamio.com>
2018-09-04 08:59:04 +0200

    use tagesspiegel config

--- config/pnn.php
+++ config/pnn.php
@@ -4,9 +4,25 @@ $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software
 $config['url']			= "https://www.pnn.de/contentexport/feed/home";
 $config['base_url']		= "https://www.pnn.de/";
 
-$config['content']		= array("#</h1>(.*)<div class=um-metabar>#Uis", 1);
+$config['content']  	= array('#<article[^>]*>(.*)</article>#Uis', 1);
 
-$config['search']		= array("#<div class=um-content-googlad id=urban-above-the-article>.*<\/div>#Uis");
-$config['replace']		= array("");
+$config['search']		= array('#<div[^>]*class="[^"]*ts-ad[^"]*"[^>]*>.*</div>#Uis',
+								'#<aside[^>]*class="[^"]*(ts-ad|ts-html-box|ts-homepage-link)[^"]*"[^>]*>.*</aside>#Uis',
+								'#<address[^>]*class="ts-authors-contact"[^>]*>.*</address>#Uis',
+								'#<figure[^>]*class="[^"]*ts-gallery[^"]*"[^>]*data-command="articleGallery"[^>]*>.*</nav>[^>]*</figure>#Uis',
+								'#<div class="ts-meta">.*</div>#Uis',
+								'#<h1[^>]*class="ts-title"[^>]*>.*</h1>#Uis',
+								'#<header[^>]*class="ts-article-header">.*<div[^>]*class="ts-subpage-header">.*</header>#Uis',
+								'#<div[^>]*class="ts-subpage-index">.*</a>.*</div>#Uis'
+								);
+$config['replace']		= array('',
+								'',
+								'',
+								'',
+								'',
+								'',
+								'',
+								''
+								);
 
 ?>
