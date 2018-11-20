57aa2446bf442f8f3f633a739b4052dd4f8641aa
Sven Fischer <git-dev@linux4tw.de>
2018-11-20 19:34:49 +0100

    fix dilbert comic links

--- config/dilbert.php
+++ config/dilbert.php
@@ -5,6 +5,8 @@ $config['author_url']	= "http://blog.strubbl.de/";
 $config['url']			= "http://dilbert.com/feed";
 $config['base_url']		= "http://dilbert.com";
 $config['content']		= array("#<div class=\"img-comic-container\">(.*)<meta itemprop=\"isFamilyFriendly\"#Uis", 1);
+$config['search'] = array("#src=\"http://dilbert.com#Uis");
+$config['replace'] = array("src=\"https:");
 $config['test_urls'] = array("http://dilbert.com/strip/2016-02-21", "http://dilbert.com/strip/2016-02-15");
 
 ?>
