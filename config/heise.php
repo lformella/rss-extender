de2ec5927c123774a34900f8fc4dc8fd07f406a9
Sven Fischer <git-dev@linux4tw.de>
2018-11-23 18:12:25 +0100

    rework heise feeds

--- config/heise.php
+++ config/heise.php
@@ -4,20 +4,32 @@ $config['author']		= "Lars Formella";
 $config['author_url']	= "http://www.larsformella.de/portfolio/programme-software/rss-extender/";
 $config['url']			= "http://www.heise.de/newsticker/heise-atom.xml";
 $config['base_url']		= "http://www.heise.de";
-$config['content'] = array("#<(div|p) (id=\"p_1\"\s+)?class=\"(meldung_wrapper|output_text|kicker_toc|article-content|article_page_intro|initial|akwa-article__content)[^>]*>(.*)</(div|p)>\s+(<!-- RSPEAK_STOP -->\s+)?(<!--googleoff: all-->\s+)?(</section>\s+)?(<footer|<div class=\"clearcontent\"></div>|<div class=\"article-footer__content\">)#Uis", 4);
-$config['search']		= array("#<span class=\"bildunterschrift\">.*<\/span>#Uis",
-  "#<(div|aside)[ ]*class=\"[^\"]*(akwa-ad-container|bcadv).*<\/(noscript|aside)>#Uis",
-  "#<span class=\"source\">.*<\/span>#Uis",
-  "#<h1>.*<\/h1>#Uis",
-  "#<!-- [a-z_-]+ -->#Uis",
+$config['content'] = array("#<article[^>]*>(.*)<\/article>#Uis", 1);
+$config['search']	= array(
+  "#<h1.*>.*<\/h1>#Uis",
   "#<script.*>.*<\/script>#Uis",
   "#<noscript>.*<\/noscript>#Uis",
-  "#<div class=\"video_titel\">[^<]*<\/div>#Uis",
-  "#<section class=\"themenseite_preisvergleich.*<\/section>#Uis",
+  "#<footer.*</footer>#Uis",
+  "#<nav.*</nav>#Uis",
   "#<a-pvg.*<\/a-pvg>#Uis",
-  "#<div class=\"a-inline-textbox.*<\/div>#Uis",
-  "#http://www.heise.de//#Uis");
-$config['replace']		= array("",
+  "#<a-tabs class=\"a-pvgs\">.*</a-tabs>#Uis",
+  "#<div class=\"article-actions\">.*</div>#Uis",
+  "#<div class=\"a-article-teaser.*</div>#Uis",
+  "#<div class=\"akwa.*</div>#Uis",
+  "#<(div|ul) class=\"article_tags.*</(div|ul)>#Uis",
+  "#<section class=\"article-sidebar\">.*</section>#Uis",
+  "#<section class=\"rectangle_ad.*</section>#Uis",
+  "#<section class=\"teaser.*</section>#Uis",
+  "#<section class=\"a-grid.*</section>#Uis",
+  "#<(figure|div|section|h3) class=\"a-x-card.*</(figure|div|section|h3)>#Uis",
+  "#<aside.*</aside>#Uis",
+  "#<header class=\"title\">.*</header>#Uis",
+  "#<p class=\"forum\">.*</p>#Uis",
+  "#<a href=\".*\" class=\"comment\" name=\"spalte.mac-and-i.forenbeitrag.[0-9]+\">.*</a>#Us",
+  "#<div id=\"zett_kasten.*</div>#Uis",
+  "#http://www.heise.de//#Uis"
+  );
+$config['replace'] = array(
   "",
   "",
   "",
@@ -28,6 +40,18 @@ $config['replace']		= array("",
   "",
   "",
   "",
-  "//");
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "//"
+  );
 
 ?>
